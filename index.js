b4e5d7219c8a51e170bcb6acfd2f314ddc35890c
EddieVilla <EddieVilla@users.noreply.github.com>
2017-09-03 16:56:53 -0500

    add index.js, calc age (years including decimals)

---
+++ index.js
@@ -0,0 +1,21 @@
+// TODO currently off by about 28-30 days
+// DONE month off by one (start from 0 as january)
+
+var xVal = ((new Date(Date.now()))-(new Date(1995,3,7,6,11)));
+var xElement = null;
+var ms = 9; //interval between UI updates
+
+xElement = document.getElementById("test");
+
+//run `startWatch` every `ms` milliseconds
+var Interval = window.setInterval(startWatch, ms);
+
+
+function startWatch(){
+
+    xVal += ms;
+
+    //update UI
+    xElement.innerHTML = xVal/1000/60/60/24/365; //convert to year format
+
+}
